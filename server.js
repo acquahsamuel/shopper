8f0ae8b4a34d277ea5153175e0609b29a78f4c97
acquahsamuel <acquahsamuel1996@gmail.com>
2021-04-04 21:28:16 +0000

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const path = require('path');
+const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 52000;
 
-app.listen(PORT, () =>{
-    console.log(`App is listening on ${PORT} `)
+//Importing files
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+//@Middlewares
+app.use(shopRoutes);
+app.use("admin", adminRoutes);
+
+
+//@des Serving Static files
+app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views" , "404.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port} `);
 });
 
