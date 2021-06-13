412cca47cdf605549dfb1c336008b83c1c2db56b
acquahsamuel <acquahsamuel1996@gmail.com>
2021-06-13 05:51:21 +0000

    [routes] admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,16 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+const adminController = require("../controllers/admin");
 
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddproduct);
+
+router.get("/products", adminController.getProducts);
+
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
+
